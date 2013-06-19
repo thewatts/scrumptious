866c403ba4ec1fb5d445f59eb48f6eb0ebcd1e61
Nathaniel Watts <reg@nathanielwatts.com>
2013-06-19 03:45:11 -0500

    add sorting on the table that displays bookmarks

--- public/js/app/controllers/bookmarks.js
+++ public/js/app/controllers/bookmarks.js
@@ -1,1 +1,10 @@
-App.BookmarksController = Ember.ArrayController.extend();
+App.BookmarksController = Ember.ArrayController.extend({
+  pushSort: function(attribute) {
+    if (this.get("sortProperties.firstObject") == attribute) {
+      this.toggleProperty("sortAscending");
+    } else {
+      this.set("sortProperties", [ attribute ]);
+      this.set("sortAscending", true);
+    }
+  }
+});
--- public/js/app/models/bookmark.js
+++ public/js/app/models/bookmark.js
@@ -1,4 +1,10 @@
 App.Bookmark = DS.Model.extend({
   title: DS.attr('string'),
-  url:   DS.attr('string')
+  url:   DS.attr('string'),
+  favorite: DS.attr('boolean'),
+
+  changedFavorite: function() {
+    this.get("transaction").commit();
+    console.log("Transaction was commited.");
+  }.observes("favorite")
 });
